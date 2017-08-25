fb009cdb7f5bc90a693db20bb1b0cebdba556b7c
Hector Flores Rodriguez <hfloresr@uci.edu>
2017-08-26 01:25:37 +0300

    pyqt5 and vtk

--- brainviz/brainviz.py
+++ brainviz/brainviz.py
@@ -1,56 +1,84 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import os
+import sys
 import numpy as np
 import vtk
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+from PyQt5.QtWidgets import (QApplication, QWidget, QMainWindow, QGridLayout,
+                             QSlider, QHBoxLayout, QVBoxLayout)
+from PyQt5.QtCore import QObject, Qt, pyqtSignal
 
-# Helper functions
-def vtk_show(renderer, width=400, height=300):
-    renderWindow = vtk.vtkRenderWindow()
-    renderWindow.SetOffScreenRendering(1)
-    renderWindow.AddRenderer(renderer)
-    renderWindow.SetSize(width, height)
-    renderWindow.Render()
+class UI(object):
+    def setup_ui(self, main_window):
+        main_window.setObjectName("MainWindow")
+        main_window.resize(600, 600)
+        self.centralWidget = QWidget(main_window)
+        self.gridlayout = QGridLayout(self.centralWidget)
+        self.vtkWidget = QVTKRenderWindowInteractor(self.centralWidget)
+        self.gridlayout.addWidget(self.vtkWidget, 0, 0, 1, 1)
+        main_window.setCentralWidget(self.centralWidget)
 
-    win2imgfilter = vtk.vtkWindowToImageFilter()
-    win2imgfilter.SetInput(renderWindow)
-    win2imgfilter.Update()
-
-    writer = vtk.vtkPNGWriter()
-    writer.SetWriteToMemory(1)
-    writer.SetInputconnection(win2imgfilter.GetOutputPort())
-    writer.Write()
-
-    return writer.GetResult()
-
-def createDummyRenderer():
-    renderer = vtk.vtkRenderer()
-    renderer.SetBackground(1.0, 1.0, 1.0)
-
-    camera = renderer.MakeCamera()
-    camera.SetPosition(-256, -256, 512)
-    camera.SetFocalPoint(0.0, 0.0, 255.0)
-    camera.SetViewAngle(30.0)
-    camera.SetViewUp(0.46, -0.80, -0.38)
-    renderer.SetActiveCamera(camera)
-
-    return renderer
-
-
-# path to the .mha file
-fname_seg = '../nac_brain_atlas/brain_segmentation.mha'
-
-reader = vtk.vtkMetaImageReader()
-reader.SetFileName(fname_seg)
-
-import pdb; pdb.set_trace()
-cast_filter = vtk.vtkImageCast()
-cast_filter.SetInputConnection(reader.GetOutputPort())
-cast_filter.SetOutputScalarTypeToUnsignedShort()
-cast_filter.Update()
-
-imdata_brain = cast_filter.GetOutput()
+        sld = QSlider(Qt.Horizontal, main_window)
+        sld.setFocusPolicy(Qt.NoFocus)
+        sld.setRange(1, 300)
+        sld.setValue(1)
+        sld.setGeometry(30, 40, 150, 30)
 
 
 
-x = 3
+class SimpleView(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.ui = UI()
+        self.ui.setup_ui(self)
+        self.ren = vtk.vtkRenderer()
+        self.ui.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.ui.vtkWidget.GetRenderWindow().GetInteractor()
+
+        # Get source
+        lh_fname = '../all_ply/pial_Full/lh.pial.ply'
+        rh_fname = '../all_ply/pial_Full/rh.pial.ply'
+
+        lhreader = vtk.vtkPLYReader()
+        lhreader.SetFileName(lh_fname)
+
+        rhreader = vtk.vtkPLYReader()
+        rhreader.SetFileName(rh_fname)
+
+        #lhreader.Update()
+        #rhreader.Update()
+
+        # Mapper
+        lhmapper = vtk.vtkPolyDataMapper()
+        lhmapper.SetInputConnection(lhreader.GetOutputPort())
+        rhmapper = vtk.vtkPolyDataMapper()
+        rhmapper.SetInputConnection(rhreader.GetOutputPort())
+
+        # Actor
+        lhactor = vtk.vtkActor()
+        lhactor.SetMapper(lhmapper)
+        rhactor = vtk.vtkActor()
+        rhactor.SetMapper(rhmapper)
+
+        self.ren.SetGradientBackground(True)
+        self.ren.AddActor(lhactor)
+        self.ren.AddActor(rhactor)
+
+        # TODO: Fix Axes
+        #axes = vtk.vtkAxesActor()
+        #a = vtk.vtkOrientationMarkerWidget()
+        #a.SetOrientationMarker(axes)
+        #a.SetInteractor(self.iren)
+        #a.EnabledOn()
+        #a.InteractiveOn()
+        #self.ren.ResetCamera()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = SimpleView()
+    window.show()
+    window.iren.Initialize()  # Need this line to show the render insie Qt
+    sys.exit(app.exec_())
---
+++ vtk_test.py
@@ -0,0 +1,84 @@
+import numpy as np
+import vtk
+from vtk.numpy_interface import dataset_adapter as dsa
+
+
+lh_fname = 'all_ply/pial_Full/lh.pial.ply'
+rh_fname = 'all_ply/pial_Full/rh.pial.ply'
+
+lhreader = vtk.vtkPLYReader()
+lhreader.SetFileName(lh_fname)
+
+rhreader = vtk.vtkPLYReader()
+rhreader.SetFileName(rh_fname)
+
+lhreader.Update()
+rhreader.Update()
+
+lh_data = lhreader.GetOutput()
+rh_data = rhreader.GetOutput()
+
+# points are (x, y, z)
+lh_points = [lh_data.GetPoint(i) for i in range(lh_data.GetNumberOfPoints())]
+rh_points = [rh_data.GetPoint(i) for i in range(rh_data.GetNumberOfPoints())]
+
+import pdb; pdb.set_trace()
+
+
+chnradius = 3.0
+chnheight = 12.0
+
+
+ch1 = vtk.vtkConeSource()
+ch1.SetResolution(60)
+ch1.SetRadius(chnradius)
+ch1.SetHeight(chnheight)
+ch1.SetCenter(30., 20., 40.)  # TODO: more channels
+ch1.SetDirection(0., 0., -1.)
+
+mlh = vtk.vtkPolyDataMapper()
+mlh.SetInputConnection(lhreader.GetOutputPort())
+
+mrh = vtk.vtkPolyDataMapper()
+mrh.SetInputConnection(rhreader.GetOutputPort())
+
+mch1 = vtk.vtkPolyDataMapper()
+mch1.SetInputConnection(ch1.GetOutputPort())
+
+alh = vtk.vtkActor()
+alh.SetMapper(mlh)
+#alh.GetProperty().SetOpacity(0.6)
+
+arh = vtk.vtkActor()
+arh.SetMapper(mrh)
+#arh.GetProperty().SetOpacity(0.65)
+
+ach1 = vtk.vtkActor()
+ach1.SetMapper(mch1)
+ach1.GetProperty().SetDiffuseColor(0.333, 0.0, 0.0)
+
+axes = vtk.vtkAxesActor()
+a = vtk.vtkOrientationMarkerWidget()
+a.SetOrientationMarker(axes)
+
+ren = vtk.vtkRenderer()
+ren.SetGradientBackground(True)
+ren.AddActor(alh)
+ren.AddActor(arh)
+ren.AddActor(ach1)
+
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+renWin.SetSize(600, 600)
+
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+a.SetInteractor(iren)
+a.EnabledOn()
+a.InteractiveOn()
+
+ren.ResetCamera()
+iren.Initialize()
+renWin.Render()
+iren.Start()
